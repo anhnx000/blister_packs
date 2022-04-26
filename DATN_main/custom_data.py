43f1e4aef9409444e678735231d55c2dfe45049f
baoanh1310 <dataholicguy@gmail.com>
2022-04-27 03:27:24 +0700

    first commit

---
+++ DATN_main/custom_data.py
@@ -0,0 +1,41 @@
+import torch
+import torchvision
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+from zipfile import ZipFile
+import pandas as pd
+from skimage import io, transform
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+import cv2
+
+class CustomDataset_xa(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.image_path_list = []
+        self.label_list = []
+
+        folder_list = os.listdir(root_dir)
+        for folder_name in folder_list:
+            rel_path = os.path.join(root_dir, folder_name)
+            for file in os.listdir(rel_path):
+                self.image_path_list.append(os.path.join(rel_path, file))
+                self.label_list.append(folder_name)
+    
+    
+    def __len__(self):
+        return len(self.image_path_list)
+    
+    def __getitem__(self, idx):
+        img_path = self.image_path_list[idx]
+        img = cv2.imread(img_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        label = self.label_list[idx]
+        if self.transform:
+            img = self.transform(img)
+        return img, int(label)
+        
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # blister_packs
 blister packs detection
+
+Cách dùng tmux:
+F2: tạo window mới
+F4: chuyển giữa các window
+Tạo session mới: tmux new -t <new_sesion_name>
+Vào lại session: tmux a -t <sesion_name>
